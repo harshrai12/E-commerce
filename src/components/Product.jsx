15633b6c3b0ee349295580b72e7f58ca9850695f
Harsh Rai <harshpayal3252@gmail.com>
2023-11-20 16:00:22 +0530

    resposive

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -23,31 +23,36 @@ const Product = ({ id, name, price, image }) => {
 
   return (
     <div className="px-6 py-4 flex flex-col justify-between h-full">
-    <div className="flex-grow">
-      {/* Content above */}
-      <img className="w-full object-contain h-64" src={image} alt={name} />
-      <h3 className="font-bold text-xl mb-2">{name}</h3>
-      <p className="text-gray-700">Rs:{price}</p>
-    </div>
-    <div className="flex-shrink-0">
-      {/* Buttons at the bottom */}
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
-        onClick={handleAddToCart}
-      >
-        Order
-      </button>
-      <Link to={`/product/${id}`}>
-        <button className="mt-2 bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray">
-          Read more
+      <div className="flex-grow">
+        {/* Content above */}
+        <img
+          className="w-full object-contain h-48 md:h-80 lg:h-96"
+          src={image}
+          alt={name}
+        />
+        <h3 className="font-bold text-xl mb-2">{name}</h3>
+        <p className="text-gray-700">Rs:{price}</p>
+      </div>
+      <div className="flex-shrink-0">
+        {/* Buttons at the bottom */}
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+          onClick={handleAddToCart}
+        >
+          Order
         </button>
-      </Link>
+        <Link to={`/product/${id}`}>
+          <button className="mt-2 bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline-gray">
+            Read more
+          </button>
+        </Link>
+      </div>
     </div>
-  </div>
-  
   );
 };
 
 export default Product;
+
+
 
 
